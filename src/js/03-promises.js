16be22ce7417ffd6bd46da1e032753b84b7fc6a1
Evgeniy <evgnec@gmail.com>
2023-04-23 21:39:37 +0300

    add

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from "notiflix";
+
+
+const form = document.querySelector('.form')
+
+function createPromise(position, newDelay) {
+  const shouldResolve = Math.random() > 0.5;
+    return new Promise((resolve, reject) => {
+
+        if (shouldResolve) {
+         return resolve({position, newDelay})
+        } else {
+         return reject({position, newDelay})
+        }
+  })
 }
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const form = e.currentTarget
+  let delay = +form['delay'].value
+  const amount = +form['amount'].value
+  const step = +form['step'].value
+  let position = 0
+
+    for (let i = 1; i <= amount; i += 1) {
+      position = i
+
+      const firstDelay = delay
+      let newDelay = delay += step
+
+      createPromise(position, firstDelay, newDelay)
+        .then(({ position, newDelay }) => {
+
+          setTimeout(() => {
+            Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${newDelay}ms`);
+          }, newDelay)
+        })
+
+        .catch(({ position, newDelay }) => {
+          setTimeout(() => {
+            Notiflix.Notify.warning(`❌ Rejected promise ${position} in ${newDelay}ms`);
+          }, newDelay)
+        })
+    }
+})
